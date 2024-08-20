3aaa8d97d266f3575a01a2a6213c395b5a64ab33
Alakinnn <s3978452@rmit.edu.vn>
2024-08-20 22:21:24 +0700

    feat(ui): added reward selection after clearing a stage

--- Slay The 11th/Slay The 11th/StageView.swift
+++ Slay The 11th/Slay The 11th/StageView.swift
@@ -8,24 +8,53 @@
 import SwiftUI
 
 struct StageView: View {
-    private var vm = GameViewModel()
+    var vm = GameViewModel()
 
     var body: some View {
         VStack(spacing: 0) {
-          EnemyZoneView(vm: vm)
-            .frame(height: UIScreen.main.bounds.height * 0.4)
+            EnemyZoneView(vm: vm)
+                .frame(height: UIScreen.main.bounds.height * 0.4)
             
-          PlayerZoneView(vm: vm)
-                          .frame(height: UIScreen.main.bounds.height * 0.25)
-          
-          PlayerHandView(vm: vm)
+            PlayerZoneView(vm: vm)
+                .frame(height: UIScreen.main.bounds.height * 0.25)
+            
+            PlayerHandView(vm: vm)
                 .frame(height: UIScreen.main.bounds.height * 0.35)
         }
         .edgesIgnoringSafeArea(.all)
+        .overlay(
+            Group {
+                if vm.stageViewModel.isShowingRewards {
+                    RewardSelectionView(
+                        rewards: RewardSystem.rewardsForStage(vm.stageViewModel.currentStage),
+                        selectedReward: Binding(
+                            get: { vm.stageViewModel.selectedReward },
+                            set: { vm.stageViewModel.selectedReward = $0 }
+                        ),
+                        onConfirm: {
+                            if let reward = vm.stageViewModel.selectedReward {
+                                RewardSystem.applyReward(reward, to: vm.stageViewModel.player, in: vm.stageViewModel)
+                                vm.stageViewModel.isShowingRewards = false
+                                vm.stageViewModel.checkAndAdvanceStage()
+                            }
+                        }
+                    )
+                    .background(Color.black.opacity(0.8))
+                    .edgesIgnoringSafeArea(.all)
+                    .transition(.opacity)
+                }
+            }
+        )
+        .onChange(of: vm.stageViewModel.isStageCompleted) {
+            if vm.stageViewModel.isStageCompleted {
+                vm.stageViewModel.isShowingRewards = true
+            }
+        }
     }
 }
 
 #Preview {
   StageView()
 }
+
 
